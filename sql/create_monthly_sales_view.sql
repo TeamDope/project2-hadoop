e7ee1bdffe976ce1076e2c67279b9a848a677247
yqian575 <61438792+yqian575@users.noreply.github.com>
2020-04-07 16:44:21 -0500

    update -- aggregate by customer & month

--- sql/create_monthly_sales_view.sql
+++ sql/create_monthly_sales_view.sql
@@ -12,7 +12,9 @@ SET VAR:database_name=dope_sales_raw;
 
 CREATE VIEW IF NOT EXISTS ${var:database_name}.customer_monthly_sales_2019_view as
 
-Select c.customer_id,c.last_name,c.first_name, year(s.order_date) as year, month(s.order_date) as month,sum(s.quantity) over (partition by month(s.order_date) ) as agg_amount
-From ${var:database_name}.customers c join ${var:database_name}.orders s on (c.customer_id = s.customer_id) where year(s.order_date)=2019 
-
-
+Select c.customer_id,c.last_name,c.first_name, year(s.order_date) as year, month(s.order_date) as month,sum(s.quantity) as agg_amount 
+From ${var:database_name}.customers c join ${var:database_name}.orders s on (c.customer_id = s.customer_id)  
+group by c.customer_id,c.last_name,c.first_name, year(s.order_date), month(s.order_date)
+having year(s.order_date)=2019
+order by c.customer_id,month(s.order_date) asc
+;
