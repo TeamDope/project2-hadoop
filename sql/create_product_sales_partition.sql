7bc1f9b4a5e3bd8c57babe534de1ba07cd93921d
Ansh <ansh.vansh712@gmail.com>
2020-04-11 12:41:33 -0700

    Changed from regex to month/year function

--- sql/create_product_sales_partition.sql
+++ sql/create_product_sales_partition.sql
@@ -21,6 +21,6 @@ AS SELECT o.*, p.name, p.price FROM ${VAR:database_name}.orders o JOIN ${VAR:dat
 CREATE TABLE IF NOT EXISTS ${VAR:database_name}.product_sales_partition PARTITIONED BY (sales_year, sales_month)
 COMMENT 'Parquet sales table partitioned by year and month'
 STORED AS Parquet
-AS SELECT order_id, sales_person_id, customer_id, product_id, name, price, quantity, price * quantity AS total_sales_amount, order_date, regexp_extract(order_date, '([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]', 1) AS sales_year, regexp_extract(order_date, '([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]', 2) AS sales_month FROM ${VAR:database_name}.denormalized_sales_products_table; 
+AS SELECT order_id, sales_person_id, customer_id, product_id, name, price, quantity, price * quantity AS total_sales_amount, order_date, year(order_date) AS sales_year, month(order_date) AS sales_month FROM ${VAR:database_name}.denormalized_sales_products_table; 
 
 
