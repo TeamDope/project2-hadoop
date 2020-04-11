e98c9ad4803a9c806d6002edcd724f6f01336a6e
CodyPerakslis <codyperakslis@gmail.com>
2020-04-11 04:57:58 -0700

    simple parquet

--- sql/create_parquet_tables.sql
+++ sql/create_parquet_tables.sql
@@ -15,5 +15,35 @@ CREATE TABLE IF NOT EXISTS ${var:database}.employees
 COMMENT 'Parquet employees table'
 STORED AS Parquet
 AS
-SELECT e.employee_id, e.first_name, e.middle_initial, e.last_name, UPPER(e.region) 
+SELECT employee_id, first_name, middle_initial, last_name, UPPER(region) 
 from ${var:source_database}.employees;
+
+
+--Create Parquet Customers Table
+
+CREATE TABLE IF NOT EXISTS ${var:database}.customers
+COMMENT 'Parquet customers table'
+STORED AS Parquet
+AS
+SELECT cutomer_id, first_name, middle_initial, last_name
+from ${var:source_database}.customers;
+
+
+--Create Parquet Orders Table
+
+CREATE TABLE IF NOT EXISTS ${var:database}.orders
+COMMENT 'Parquet orders table'
+STORED AS Parquet
+AS
+SELECT order_id, sales_person_id, customer_id, product_id, quantity, order_date
+from ${var:source_database}.orders;
+
+
+--CREATE Parquet Products Tables
+
+CREATE TABLE IF NOT EXISTS ${var:database}.products
+COMMENT 'PARQUET products table'
+STORED AS Parquet
+AS
+SELECT product_id, name, price
+from ${var:source_database}.products;
